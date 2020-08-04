a4ea16a1e4a32f06a3d43bf1b86dd9f59434a704
longnguyen2010 <l-nguyen@dimage.co.jp>
2020-08-04 17:03:20 +0900

    fix crawl file

--- index.js
+++ index.js
@@ -6,77 +6,115 @@ const fs = require('fs');
   const browser = await puppeteer.launch({headless: false});
   const page = await browser.newPage();
 
-  await page.goto('https://kantan.vn//grammar-20.htm');
+  const pageurl = 'https://kantan.vn//grammar-{index}.htm'
 
-  const getData = await page.evaluate(() => {
+  const sleepFunction =　async function sleep(t) {
+    return await new Promise(r => {
+      setTimeout(() => {
+        r();
+      }, t);
+    });
+  }
+
+  var crawlUrl
+  var grammars = []
+  for (let index = 11; index < 2700; index++) {
+
+    crawlUrl = pageurl.replace('{index}', index)
+    await page.goto(crawlUrl)
+
+    const getData = await page.evaluate(() => {
+
       const getContentFromChild = (parentElement, childClassName) => {
-      let childElement = parentElement.querySelector(childClassName)
-      if (childElement != null) {
-        return childElement.innerText.trim()
-      } else {
-        return ''
-      }
-    }
-
-    const getExamples = (parentElement, childClass) => {
-      var examples = []
-      var exampleElements = parentElement.querySelectorAll(childClass)
-      exampleElements.forEach(element => {
-        var japanesesText = getContentFromChild(element, 'span.grammar-ex-japan')
-        var vietnameseText = getContentFromChild(element, 'p')
-        if ((japanesesText !== '') && (vietnameseText !== '')) {
-            examples.push({
-              japanese: japanesesText,
-              vietnamese: vietnameseText
-            })
+        let childElement = parentElement.querySelector(childClassName)
+        if (childElement != null) {
+          return childElement.innerText.trim()
+        } else {
+          return ''
         }
-      })
-      return examples
-    }
-    let popupElements = document.getElementsByClassName('dekiru-popup-detail')
-    if(popupElements.length == 0) {
-      return
-    }
-    let popupElement = popupElements[0]
+      }
+  
+      const getExamples = (parentElement, childClass) => {
+        var examples = []
+        var exampleElements = parentElement.querySelectorAll(childClass)
+        exampleElements.forEach(element => {
+          var japanesesText = getContentFromChild(element, 'span.grammar-ex-japan')
+          var vietnameseText = getContentFromChild(element, 'p')
+          if ((japanesesText !== '') && (vietnameseText !== '')) {
+              examples.push({
+                japanese: japanesesText,
+                vietnamese: vietnameseText
+              })
+          }
+        })
+        return examples
+      }
 
-    // title 
-    const gmwWrap = popupElement.querySelector('div.gmw-wrap')
+      let grammar = {
+        level: '',
+        grammarText: '',
+        shortMeanText: '',
+        usingMethod: '',
+        detailExplain: '',
+        examples: [],
+        url: window.location.href
+      }
 
-    let grammarText = getContentFromChild(gmwWrap, 'p.gram')
+      let popupElements = document.getElementsByClassName('dekiru-popup-detail')
+      if(popupElements.length == 0) {
+        return grammar
+      }
+      let popupElement = popupElements[0]
 
-    let meanText =　getContentFromChild(gmwWrap, 'p.mean')
+      if (popupElement == null) {
+        return grammar
+      }
+  
+      // title 
+      const gmwWrap = popupElement.querySelector('div.gmw-wrap')
+  
+      let grammarText = getContentFromChild(gmwWrap, 'p.gram')
+  
+      let meanText =　getContentFromChild(gmwWrap, 'p.mean')
+  
+      let usingMethod = getContentFromChild(popupElement, 'div.wpgam-det')
+  
+      let detailExplain = getContentFromChild(popupElement, 'div.grd-div')
+  
+      let examplesElement = popupElement.querySelector('ul.grd-ul')
+  
+      let examples = getExamples(examplesElement, 'li')
+  
+      let level = getContentFromChild(popupElement, 'div.swiper-wrapper > div.related-grammar-item > div.kj-n > span.level')
+  
+      grammar['level'] = level
 
-    let usingMethod = getContentFromChild(popupElement, 'div.wpgam-det')
+      grammar['grammarText'] = grammarText
 
-    let detailExplain = getContentFromChild(popupElement, 'div.grd-div')
+      grammar['shortMeanText'] = meanText
 
-    let examplesElement = popupElement.querySelector('ul.grd-ul')
+      grammar['usingMethod'] = usingMethod
 
-    let examples = getExamples(examplesElement, 'li')
+      grammar['detailExplain'] = detailExplain
 
-    let level = getContentFromChild(popupElement, 'div.swiper-wrapper > div.related-grammar-item > div.kj-n > span.level')
-
-    let grammar = {
-      level: level,
-      grammarText: grammarText,
-      shortMeanText: meanText,
-      usingMethod: usingMethod,
-      detailExplain: detailExplain,
-      examples: examples,
-    }
-
-    return grammar
-  })
-
-  let result = JSON.stringify(getData, null, 2)
-
-  fs.readFile('grammars.json', function (err, data) {
-    var json = JSON.parse(data)
-    json.push(result)
-    fs.writeFile("grammars.json", JSON.stringify(json), function(err) {
-      if (err) throw err;
-      console.log('completed')
+      grammar['examples'] = examples
+      
+      return grammar
     })
+
+    let result = JSON.stringify(getData, null, 2)
+
+    grammars.push(result)
+
+    await sleepFunction(15000);
+  }
+
+  console.log('result', grammars)
+
+  fs.writeFile("grammars.json", JSON.stringify(grammars), function(err) {
+    if (err) throw err;
+    console.log('completed')
   })
+
   await browser.close();
 })();
